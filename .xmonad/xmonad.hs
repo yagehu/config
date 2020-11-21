8f2dea24948ea11bb368a5a2226ba5e65128f7ad
Yage Hu <yagejhu@gmail.com>
2020-11-21 13:20:17 -0800

    .

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,19 +4,24 @@ import System.IO
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout.Spacing
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Util.SpawnOnce
 
 main = do
   xmonad $ docks defaultConfig
-    { layoutHook  = avoidStruts $ layoutHook defaultConfig
+    { layoutHook  = myLayoutHook $ avoidStruts $ layoutHook defaultConfig
 	, terminal    = "alacritty"
     , modMask     = mod4Mask
     , startupHook = myStartupHook <+> startupHook defaultConfig
 	, keys        = \c -> myKeys c `M.union` keys defaultConfig c
     }
   where
+    myLayoutHook = spacingRaw
+	  False
+	  (Border 10 10 10 10) True
+	  (Border 10 10 10 10) True
     myKeys (XConfig {modMask = mod4Mask}) = M.fromList $
       [ ((mod4Mask, xK_b), sendMessage ToggleStruts)
       ]
